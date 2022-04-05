acc21d8a77cde85964a1b6231fdefd871d9ba1b1
Dominique Dias <ddias@workmedia.pt>
2022-04-05 15:07:11 +0100

    new style

---
+++ src/Components/Info.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export function About() {
+  return (
+      <div className="about">
+        <button className="btn-about">About</button>
+        <p className="p-about">Content</p>
+      </div>
+  );
+}
+
+export function Experience() {
+    return (
+        <div className="experience">
+          <button className="btn-experience">Experience</button>
+          <p className="p-experience">Content</p>
+        </div>
+    );
+  }
+
+  export function Skills() {
+    return (
+        <div className="skills">
+          <button className="btn-skills">Skills</button>
+          <p className="p-skills">Content</p>
+        </div>
+    );
+  }
