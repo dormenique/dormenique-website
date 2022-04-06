00688bad2338701c133debdd4212a869db5e5590
dormenique <dormenique@gmail.com>
2022-04-06 22:41:25 +0100

    fixed style card, footer, social media

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -1,13 +1,25 @@
 import React from "react";
-import './SocialMedia.css'
+import "./SocialMedia.css";
 
 export default function SocialMedia() {
   return (
     <div className="social-media">
-      <a href="http://github.com" target="_blank" rel="noreferrer"><img src="/github-icon.svg" alt="github icon"/></a>
-      <a href="http://github.com" target="_blank" rel="noreferrer"><img src="/linkedin-icon.svg" alt="linkedin icon" /></a>
-      <a href="http://github.com" target="_blank" rel="noreferrer"><img src="/medium-icon.svg" alt="medium icon" /></a>
-      <a href="http://github.com" target="_blank" rel="noreferrer"><img src="/instagram-icon.svg" alt="instagram icon" /></a>
+      <a href="http://github.com" target="_blank" rel="noreferrer">
+        <img src="/github-icon.svg" alt="github icon" />
+        github
+      </a>
+      <a href="http://github.com" target="_blank" rel="noreferrer">
+        <img src="/linkedin-icon.svg" alt="linkedin icon" />
+        linkedin
+      </a>
+      <a href="http://github.com" target="_blank" rel="noreferrer">
+        <img src="/medium-icon.svg" alt="medium icon" />
+        medium
+      </a>
+      <a href="http://github.com" target="_blank" rel="noreferrer">
+        <img src="/instagram-icon.svg" alt="instagram icon" />
+        instagram
+      </a>
     </div>
   );
 }
